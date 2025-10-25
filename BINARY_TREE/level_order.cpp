b837912c02965f77657613bc71fb0e1f266d7270
SUVRAAJ <nandsuvraaj@gmail.com>
2025-10-26 02:04:55 +0530

    done binaru trees

--- BINARY_TREE/level_order.cpp
+++ BINARY_TREE/level_order.cpp
@@ -2,6 +2,12 @@
 #include <queue>
 using namespace std;
 
+class dh{
+    public:
+    int height;
+    int diameter;
+};
+
 class node{
     public:
     node* left;
@@ -71,10 +77,66 @@ void level_order_traversal(node* root)
     }
 }
 
+int height_tree(node* root)
+{
+    if(root==nullptr)
+    {
+        return 0;
+    }
+
+    int h_left=height_tree(root->left);
+    int h_right=height_tree(root->right);
+
+    return 1+max(h_left,h_right);
+}
+
+int diameter_tree(node*root)
+{
+    if(root==nullptr)
+    {
+        return 0;
+    }
+
+    int d1= height_tree(root->right)+height_tree(root->left);
+    int d2= diameter_tree(root->left);
+    int d3= diameter_tree(root->right);
+
+    return max(d1,max(d2,d3));
+}
+
+dh opt_diameter(node* root)
+{
+    dh ans;
+    if(root==nullptr)
+    {
+        ans.height=0;
+        ans.diameter=0;
+        return ans;
+    }
+
+    dh left_dia=opt_diameter(root->left);
+    dh right_dia=opt_diameter(root->right);
+    ans.height= 1+max(left_dia.height,right_dia.height);
+    
+    int d1=left_dia.height+right_dia.height;
+    int d2=left_dia.diameter;
+    int d3=right_dia.diameter;
+
+
+    ans.diameter= max(d1,max(d2,d3));
+    return ans;
+}
+
 int main()
 {
-
     node* head=pre_order_build();
     level_order_traversal(head);
+    cout<<height_tree(head)<<endl;
+    dh wow;
+    wow= opt_diameter(head);
+    cout<<wow.height<<" "<<wow.diameter<<endl;
+    cout<<diameter_tree(head);
     return 0;
 }
+
+////1 2 4 -1 -1 5 8 -1 -1 -1 3 6 -1 9 10 -1 -1 -1 7 -1 -1///
